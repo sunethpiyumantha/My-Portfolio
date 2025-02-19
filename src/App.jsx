f9dba22ccb2d05c4adfd2ae7e73f9ff588ad9271
Suneth Piyumantha <156288422+sunethpiyumantha@users.noreply.github.com>
2025-02-19 22:07:08 +0530

    1.0.1
    Navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import About from "./components/About";
+import Services from "./components/Services";
+
 const App = () => {
   return (
     <>
-      <h1>Subscribe for more videos</h1>
+      <Navbar/>
+      <Header/>
+      <About/>
+      <Services/>
     </>
   );
 };
