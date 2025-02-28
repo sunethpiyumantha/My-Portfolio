ccf26875b06cc466956e85f3116c386c73c0dd18
Suneth Piyumantha <156288422+sunethpiyumantha@users.noreply.github.com>
2025-02-28 23:31:52 +0530

    1.0.8

---
+++ src/components/Services/index.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import "./Services.css"
+import {services} from "../../data"
+import ServiceCard from '../../ui/ServiceCard'
+
+const Services = () => {
+  return (
+    <section id='services'>
+      <div className="section__wrapper">
+        <div className="section__header">
+          <h2 className="shine">Services</h2>
+        </div>
+        <div className="services__group">
+          {services.map(
+            ({ title, description, icon, color, colorRGB },index)=>(
+            <ServiceCard
+              title={title}
+              description={description}
+              icon={icon}
+              color={color}
+              colorRGB={colorRGB}
+              key={index}
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Services
