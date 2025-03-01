d761e3c6f2722b37dfef663dec568ee94a6dee5a
Suneth Piyumantha <156288422+sunethpiyumantha@users.noreply.github.com>
2025-03-01 22:42:02 +0530

    1.0.9

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import "./Projects.css"
+import { projects } from "../../data"
+import ProjectCard from '../../ui/ProjectCard'
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <section id='projects'>
+      <div className="section__wrapper">
+        <div className="section__header">
+          <h2 className="shine">Projects</h2>
+        </div>
+        <div className="projects__container">
+          {projects.map((project, index)=>(
+            <ProjectCard {...project} key={index}/>
+          ))}
+        </div>
+      </div>
+    </section>
   )
 }
 
