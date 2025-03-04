62bdd6e5052f4ce102516387dbc75c9735004d82
Suneth Piyumantha <156288422+sunethpiyumantha@users.noreply.github.com>
2025-03-04 22:39:47 +0530

    1.1.1

---
+++ src/components/Skills/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./Skills.css"
+import { skills } from "../../data"
+import SkillCard from '../../ui/SkillCard'
+
+const Skills = () => {
+  return (
+    <section id="skills">
+      <div className="section__wrapper">
+        <div className="section__header">
+          <h2 className="shine">My Skills</h2>
+        </div>
+        <div className="skill__container">
+          {skills.map((list, index)=>(
+            <SkillCard {...list} key={index}/>
+          ))}
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Skills
